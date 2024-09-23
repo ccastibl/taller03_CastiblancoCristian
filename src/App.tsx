d39318edf3266b1a2de68937ba78ef0f4b380a84
ccastibl <ccastibl@cuc.edu.co>
2024-09-23 09:41:39 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "./assets/img/logo_react.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="logo de react" />
+        <h1>Escuela de la tortuga</h1>
+        <p>Taller03 CastiblancoCristian</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
